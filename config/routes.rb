42c8ce9963b08ad09189190ce638480df2cfb05a
Austen Valletta <avallett@uncc.edu>
2020-04-20 15:45:45 +0000

    stopping, at slide update view

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+  get 'sessions/new'
+  get 'sessions/create'
+  get 'sessions/destroy'
+  resources :users
   get 'welcome/index'
   
   resources :articles do
@@ -6,5 +10,10 @@ Rails.application.routes.draw do
   end
   
   root 'welcome#index'
+  
+  resources :sessions, only: [:new, :create, :destroy]
+  get 'signup', to: 'users#new', as: 'signup'
+  get 'login', to: 'sessions#new', as: 'login'
+  get 'logout', to: 'sessions#destroy', as: 'logout'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
