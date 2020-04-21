7fcd88e56e619362f137698e62051e5f4d13358d
Austen Valletta <avallett@uncc.edu>
2020-04-21 17:53:05 +0000

    Added view counter

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -9,6 +9,7 @@ class ArticlesController < ApplicationController
     
     def show
         @article = Article.find(params[:id])
+        @article.punch(request)
     end
     
     def create
--- app/models/article.rb
+++ app/models/article.rb
@@ -1,4 +1,5 @@
 class Article < ApplicationRecord
+    acts_as_punchable
     belongs_to :user
     has_many :comments, dependent: :destroy
     validates :title, presence: true, length: { minimum: 5 }
